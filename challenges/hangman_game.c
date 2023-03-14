6862e17758b700a0847146c42f6aa880fa3cefb7
Caio Borges Sella <sellcaio4@gmail.com>
2023-03-14 16:26:52 -0300

    Update conceitos, desafios e dicas

--- challenges/hangman_game.c
+++ challenges/hangman_game.c
@@ -6,18 +6,37 @@ int main(){
 
     sprintf(palavraSecreta, "MELANCIA");
 
-    int acertou = 0;
-    int enforcou = 1;
+    int acertou = 0, enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
 
     do{
-        char chute;
-        scanf("%c", &chute);
+        for(int i = 0; i < strlen(palavraSecreta); i++){
+            int achou = 0;
 
-        for (int i = 0; i < strlen(palavraSecreta); i++) {
-            if(palavraSecreta[i] == chute) {
-                printf("A posição %d tem  essa letra!\n", i);
+            for(int j = 0; j < tentativas; j++){
+                if(chutes[j] == palavraSecreta[i]){
+                    achou = 1;
+                    break;
+                }
+            }
+
+            if(achou) {
+                printf("%c ", palavraSecreta[i]);
+            } else{
+                printf("_ ");
             }
         }
+
+        printf("\n");
+
+        char chute;
+        printf("Qual letra? ");
+        scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
         
     }while (!acertou && !enforcou);
 }
